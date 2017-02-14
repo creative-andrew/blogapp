a3dd9b22749e72b07bb6e113ee2c862e26586b63
geist.ahnen <geist.ahnen@gmail.com>
2017-02-14 09:53:52 -0500

    version terminada

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express'),
 bodyparser = require('body-parser'),
 mongoose = require('mongoose'),
+methodOverride = require('method-override'),
+expressSanitizer = require('express-sanitizer')
 app = express();
 
 mongoose.connect("mongodb://localhost/blogapp");
@@ -8,9 +10,11 @@ mongoose.connect("mongodb://localhost/blogapp");
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
 
 
 app.use(bodyparser.urlencoded({extented: true}));
+app.use(expressSanitizer());
 
 // Mongoose Model 
 var blogSchema = new mongoose.Schema({
@@ -45,6 +49,7 @@ app.get('/blogs', function (req, res) {
 
 
 app.post('/blogs', function(req, res){
+ req.body.blog.body = req.sanitize(req.body.blog.body) 
  Blog.create(req.body.blog, function(err, newBlog){
    if (err){
      console.log(err)
@@ -60,6 +65,62 @@ app.get('/blogs/new', function (req, res) {
       
           res.render('new');     
         });
+
+
+// SHOW ROUTE //
+
+app.get('/blogs/:id', function(req, res) {
+  Blog.findById(req.params.id, function(err, foundblog ){
+    if (err) {
+      console.log(err);
+    }
+    else {
+ res.render('show', {blog: foundblog});
+    }
+  })
+     
+});
+
+// EDIT ROUTE //
+
+ app.get('/blogs/:id/edit', function(req, res) {
+   Blog.findById(req.params.id, function(err, foundblog ){
+    if (err) {
+      console.log(err);
+    }
+    else {
+    res.render("edit", {blog: foundblog});
+    }
+  })
+
+ });
+
+ // UPDATE ROUTE //
+
+ app.put('/blogs/:id', function(req, res){
+   console.log(req.body.blog);
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog){
+       if (err) {
+         console.log(err)
+       }
+       else {
+         res.redirect("/blogs/"+req.params.id)
+       }
+   });
+ });
+
+ /// DELETE ROUTE ///
+
+ app.delete('/blogs/:id', function(req, res){
+   Blog.findByIdAndRemove(req.params.id, function(err){
+     if (err) {
+       console.log(err)
+     }
+     else {
+       res.redirect('/blogs');
+     }
+   })
+ });
 
 app.listen(3000, function () {
   console.log('Blog app listening on port 3000!');
